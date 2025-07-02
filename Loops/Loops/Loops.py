b673ea62e554fabdb71f04ec349fa3b1fa9bedb0
keynin14 <kenandurmss@gmail.com>
2025-07-02 20:14:31 +0300

    Add files via upload

---
+++ Loops/Loops/Loops.py
@@ -0,0 +1,31 @@
+x=0
+
+while x<=10:
+
+    print(x)
+    x+=1
+
+# This code will print numbers from 0 to 10, inclusive.
+
+#while True: infinite loop example
+
+for x in range(20):
+    print(x)
+    # This code will print numbers from 0 to 19, inclusive.
+
+for x in range(1,21):
+    print(x)
+    # This code will print numbers from 1 to 20, inclusive.
+
+
+for x in range(1,21,2):
+    print(x)
+    # This code will print odd numbers from 1 to 20, inclusive.
+
+while x<10:
+    if x==5:
+        break
+    x+=1
+    print(x)
+    # This code will print numbers from 1 to 4, inclusive, and then exit the loop when x equals 5.
+
