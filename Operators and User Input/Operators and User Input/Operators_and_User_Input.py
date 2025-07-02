b5339e2f72c9372d0d55b91bccadbe3cce3d1670
keynin14 <kenandurmss@gmail.com>
2025-07-02 19:09:58 +0300

    Add files via upload

---
+++ Operators and User Input/Operators and User Input/Operators_and_User_Input.py
@@ -0,0 +1,42 @@
+
+#ARITHMETIC OPERATORS
+print(8/2) # Division
+print(4*2) # Multiplication
+print(10-2) # Subtraction
+print(5+3) # Addition
+
+print(5**2) # Exponentiation
+print(10 % 3) # Modulus
+print(10 // 3) # Floor Division
+
+#ASSIGNMENT OPERATORS
+
+x=10
+y=20
+z=30
+t=40
+r=50
+y+=10 #y=y+10
+z-=5 #z=-5
+t*=2 #t=t*2
+r/=4 #r=r/4
+
+#COMPARISON OPERATORS
+
+a = 10
+b = 20
+print(a==b) # Equal to
+print(a<b) # Less than
+print(a>b) # Greater than
+print(a<=b) # Less than or equal to
+print(a>=b) # Greater than or equal to
+print(a!=b) # Not equal to)
+
+#USER INPUT
+
+w=input("Enter firsr number: ")
+i=input("Enter second number: ")
+w=int(w)
+i=int(i)
+
+print("Sum of two numbers is: ", w+i)
