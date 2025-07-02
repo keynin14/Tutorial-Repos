26f9cb31bedc76bb9f4ae2f2258dea30dc1639fd
kenan <kenan@KENAN>
2025-07-02 17:38:06 +0300

    Add project files.

---
+++ Variables_and_DataTypes/Variables_and_DataTypes.py
@@ -0,0 +1,34 @@
+print("Hello World!")
+
+#str "hello" 'hello' #this is string variable
+#"""
+#Multi
+#Line
+#String
+#"""
+
+#int 10 -9  #this is integer variable
+
+#float 10.5 -9.5  #this is float variable
+
+#complex(10,5) #10+5j  #this is complex variable
+
+#bool True(1) False(0)  #this is boolean variable
+
+print("10"+"10")
+print(10+10)
+
+print(type(10))
+print(type(10.5))
+print(type("10"))
+
+x=10
+y=20
+whatever=True
+
+print(x+y)
+
+x2="120"
+
+x2=int(x2)
+print(x2/3)
